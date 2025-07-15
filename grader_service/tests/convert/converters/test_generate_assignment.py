ba20359734b7b224ef989ee8a7a53392e8f45d31
florian.jaeger <florian.jaeger@tuwien.ac.at>
2025-07-15 12:47:28 +0200

    test: updated generate assignment tests to ensure notebooks are converted

--- grader_service/tests/convert/converters/test_generate_assignment.py
+++ grader_service/tests/convert/converters/test_generate_assignment.py
@@ -15,6 +15,8 @@ def test_generate_assignment(tmp_path):
     ).start()
 
     assert (output_dir / "simple.ipynb").exists()
+    # Ensure the notebook was converted and not overwritten
+    assert not "BEGIN SOLUTION" in (output_dir / "simple.ipynb").read_text()
     assert (output_dir / "gradebook.json").exists()
 
 
@@ -33,6 +35,8 @@ def test_generate_assignment_no_copy_with_files(tmp_path):
     ).start()
 
     assert (output_dir / "simple.ipynb").exists()
+    # Ensure the notebook was converted
+    assert not "BEGIN SOLUTION" in (output_dir / "simple.ipynb").read_text()
     assert (output_dir / "gradebook.json").exists()
     assert not (output_dir / "test.txt").exists()
 
@@ -47,11 +51,13 @@ def test_generate_assignment_copy_with_files(tmp_path):
         input_dir=str(input_dir),
         output_dir=str(output_dir),
         file_pattern="*.ipynb",
-        assignment_settings=AssignmentSettings(allowed_files=["*"]),
+        assignment_settings=AssignmentSettings(allowed_files=["*[!.ipynb]"]),
         config=None,
     ).start()
 
     assert (output_dir / "simple.ipynb").exists()
+    # Ensure the notebook was converted
+    assert not "BEGIN SOLUTION" in (output_dir / "simple.ipynb").read_text()
     assert (output_dir / "gradebook.json").exists()
     assert (output_dir / "test.txt").exists()
 
@@ -78,6 +84,8 @@ def test_generate_assignment_copy_with_dirs(tmp_path):
     ).start()
 
     assert (output_dir / "simple.ipynb").exists()
+    assert (output_dir / "simple.ipynb").exists()
+    # Ensure the notebook was converted
     assert (output_dir / "gradebook.json").exists()
     assert (output_dir / "dir_1").exists()
     assert (output_dir / "dir_1/dir_2").exists()
