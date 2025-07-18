e10a910affa5a561d8e188873172e8b49d2a5fe0
Natalia <natalia.maniakowska@tuwien.ac.at>
2025-07-18 15:43:14 +0200

    refactor: Use sqlalchemy ORM (not raw SQL) to update values in migration

--- grader_service/migrate/versions/9983ef1fda76_add_user_pk_id.py
+++ grader_service/migrate/versions/9983ef1fda76_add_user_pk_id.py
@@ -27,26 +27,25 @@ def upgrade():
     # 2.1. Create a new "user_id" column in "takepart"; it is nullable for now, until we fill it.
     op.add_column("takepart", sa.Column("user_id", sa.Integer(), nullable=True))
 
+    user_table = sa.table("user", sa.column("id"), sa.column("name"))
+
     # 2.2. Set the user_id values to the correct ids from the user table.
-    # TODO: Can it be done with sqlalchemy?
-    op.execute(
-        """
-            UPDATE takepart
-            SET user_id = (
-                SELECT id FROM user WHERE user.name = takepart.username
-            );
-            """
+    takepart_table = sa.table("takepart", sa.column("username"), sa.column("user_id"))
+    user_subq = (
+        sa.select(user_table.c.id)
+        .where(user_table.c.name == takepart_table.c.username)
+        .scalar_subquery()
     )
+    op.execute(takepart_table.update().values(user_id=user_subq))
 
     # 2.3. Create FK to the user id in takepart, update the PK, drop the reference to username.
     with op.batch_alter_table("takepart") as batch_op:
-        # TODO: Do we want ON DELETE CASCADE?
         batch_op.create_foreign_key(
             "fk_takepart_user_id",
             "user",
             ["user_id"],
             ["id"],
-            # ondelete="CASCADE"
+            # ondelete="CASCADE"  # TODO: Do we want ON DELETE CASCADE?
         )
         batch_op.alter_column("user_id", nullable=False)
         batch_op.create_primary_key("pk_takepart", ["user_id", "lectid"])
@@ -57,73 +56,70 @@ def upgrade():
     op.add_column("submission", sa.Column("user_id", sa.Integer(), nullable=True))
 
     # 3.2. Set the user_id values to the correct ids from the user table.
-    # TODO: Can it be done with sqlalchemy?
-    op.execute(
-        """
-        UPDATE submission
-        SET user_id = (SELECT id
-                       FROM user
-                       WHERE user.name = submission.username);
-        """
+    submission_table = sa.table("submission", sa.column("username"), sa.column("user_id"))
+    user_subq = (
+        sa.select(user_table.c.id)
+        .where(user_table.c.name == submission_table.c.username)
+        .scalar_subquery()
     )
+    op.execute(submission_table.update().values(user_id=user_subq))
 
     # 3.3. Create FK to the user id in submission, drop the reference to username.
     with op.batch_alter_table("submission") as batch_op:
-        # TODO: Do we want ON DELETE CASCADE?
         batch_op.create_foreign_key(
             "fk_submission_user_id",
             "user",
             ["user_id"],
             ["id"],
-            # ondelete="CASCADE"
+            # ondelete="CASCADE"  # TODO: Do we want ON DELETE CASCADE?
         )
         batch_op.alter_column("user_id", nullable=False)
         batch_op.drop_column("username")
 
 
 def downgrade():
+    user_table = sa.table("user", sa.column("id"), sa.column("name"))
+
     # # 3. Revert the change (username -> user_id) in "submission".
     op.add_column("submission", sa.Column("username", sa.String(length=255), nullable=True))
-    # TODO: Can it be done with sqlalchemy?
-    op.execute(
-        """
-            UPDATE submission
-            SET username = (
-                SELECT name FROM user WHERE user.id = submission.user_id
-            );
-            """
+
+    submission_table = sa.table("submission", sa.column("username"), sa.column("user_id"))
+    user_subq = (
+        sa.select(user_table.c.name)
+        .where(user_table.c.id == submission_table.c.user_id)
+        .scalar_subquery()
     )
+    op.execute(submission_table.update().values(username=user_subq))
+
     with op.batch_alter_table("submission") as batch_op:
-        # TODO: Do we want ON DELETE CASCADE?
         batch_op.create_foreign_key(
             "fk_submission_username",
             "user",
             ["user_id"],
             ["id"],
-            # ondelete="CASCADE"
+            # ondelete="CASCADE"  # TODO: Do we want ON DELETE CASCADE?
         )
         batch_op.alter_column("username", nullable=False)
         batch_op.drop_column("user_id")
 
     # 2. Revert the change (username -> user_id) in "takepart".
     op.add_column("takepart", sa.Column("username", sa.String(length=255), nullable=True))
-    # TODO: Can it be done with sqlalchemy?
-    op.execute(
-        """
-            UPDATE takepart
-            SET username = (
-                SELECT name FROM user WHERE user.id = takepart.user_id
-            );
-            """
+
+    takepart_table = sa.table("takepart", sa.column("username"), sa.column("user_id"))
+    user_subq = (
+        sa.select(user_table.c.name)
+        .where(user_table.c.id == takepart_table.c.user_id)
+        .scalar_subquery()
     )
+    op.execute(takepart_table.update().values(username=user_subq))
+
     with op.batch_alter_table("takepart") as batch_op:
-        # TODO: Do we want ON DELETE CASCADE?
         batch_op.create_foreign_key(
             "fk_takepart_username",
             "user",
             ["user_id"],
             ["id"],
-            # ondelete="CASCADE"
+            # ondelete="CASCADE"  # TODO: Do we want ON DELETE CASCADE?
         )
         batch_op.alter_column("username", nullable=False)
         batch_op.create_primary_key("pk_takepart", ["username", "lectid"])
