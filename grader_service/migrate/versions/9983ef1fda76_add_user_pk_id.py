4ef96fbe8f50378d9cd98d647a7e0b8eda82a09c
Natalia <natalia.maniakowska@tuwien.ac.at>
2025-07-21 18:32:51 +0200

    chore: Update the migration to also add user_id to `api_token`, `oauth_code` tables

--- grader_service/migrate/versions/9983ef1fda76_add_user_pk_id.py
+++ grader_service/migrate/versions/9983ef1fda76_add_user_pk_id.py
@@ -23,22 +23,31 @@ def upgrade():
         batch_op.create_primary_key("pk_user_id", columns=["id"])
         batch_op.create_unique_constraint("unique_user_name", ["name"])
 
-    # 2. Create a FK to user id in "takepart".
-    # 2.1. Create a new "user_id" column in "takepart"; it is nullable for now, until we fill it.
-    op.add_column("takepart", sa.Column("user_id", sa.Integer(), nullable=True))
-
     user_table = sa.table("user", sa.column("id"), sa.column("name"))
 
-    # 2.2. Set the user_id values to the correct ids from the user table.
-    takepart_table = sa.table("takepart", sa.column("username"), sa.column("user_id"))
-    user_subq = (
-        sa.select(user_table.c.id)
-        .where(user_table.c.name == takepart_table.c.username)
-        .scalar_subquery()
-    )
-    op.execute(takepart_table.update().values(user_id=user_subq))
+    def _add_user_id_col(table_name: str) -> None:
+        """A helper method that adds a `user_id` column and fills it based on `username`."""
 
-    # 2.3. Create FK to the user id in takepart, update the PK, drop the reference to username.
+        # The new column is nullable for now - until we fill it.
+        op.add_column(table_name, sa.Column("user_id", sa.Integer(), nullable=True))
+
+        table = sa.table(table_name, sa.column("username"), sa.column("user_id"))
+        user_subq = (
+            sa.select(user_table.c.id)
+            .where(user_table.c.name == table.c.username)
+            .scalar_subquery()
+        )
+        op.execute(table.update().values(user_id=user_subq))
+
+        with op.batch_alter_table(table_name) as batch_op:
+            batch_op.alter_column("user_id", nullable=False)
+
+    # 2. Create a FK to user id in the "takepart" table.
+    # 2.1. Create a new "user_id" column in "takepart" and fill its values based on the
+    # "username" column.
+    _add_user_id_col("takepart")
+
+    # 2.2. Create FK to the user id in "takepart", update the PK, drop the "username" column.
     with op.batch_alter_table("takepart") as batch_op:
         batch_op.create_foreign_key(
             "fk_takepart_user_id",
@@ -47,24 +56,15 @@ def upgrade():
             ["id"],
             # ondelete="CASCADE"  # TODO: Do we want ON DELETE CASCADE?
         )
-        batch_op.alter_column("user_id", nullable=False)
         batch_op.create_primary_key("pk_takepart", ["user_id", "lectid"])
         batch_op.drop_column("username")
 
-    # 3. Create a FK to user id in "submission".
-    # 3.1. Create a new "user_id" column in "submission"; it is nullable for now, until we fill it.
-    op.add_column("submission", sa.Column("user_id", sa.Integer(), nullable=True))
+    # 3. Create a FK to user id in the "submission" table.
+    # 3.1. Create a new "user_id" column in "submission" and fill its values based on the
+    # username" column.
+    _add_user_id_col("submission")
 
-    # 3.2. Set the user_id values to the correct ids from the user table.
-    submission_table = sa.table("submission", sa.column("username"), sa.column("user_id"))
-    user_subq = (
-        sa.select(user_table.c.id)
-        .where(user_table.c.name == submission_table.c.username)
-        .scalar_subquery()
-    )
-    op.execute(submission_table.update().values(user_id=user_subq))
-
-    # 3.3. Create FK to the user id in submission, drop the reference to username.
+    # 3.2. Create FK to the user id in "submission", drop the "username" column.
     with op.batch_alter_table("submission") as batch_op:
         batch_op.create_foreign_key(
             "fk_submission_user_id",
@@ -73,23 +73,83 @@ def upgrade():
             ["id"],
             # ondelete="CASCADE"  # TODO: Do we want ON DELETE CASCADE?
         )
-        batch_op.alter_column("user_id", nullable=False)
+        batch_op.drop_column("username")
+
+    # 4. Create a FK to user id in the "api_token" table.
+    # 4.1. Create a new "user_id" column in "api_token" and fill its values based on the
+    # "username" column.
+    _add_user_id_col("api_token")
+
+    # 4.2. Create FK to the user id in "api_token", drop the "username" column.
+    with op.batch_alter_table("api_token") as batch_op:
+        batch_op.create_foreign_key(
+            "fk_api_token_user_id",
+            "user",
+            ["user_id"],
+            ["id"],
+            # ondelete="CASCADE"  # TODO: Do we want ON DELETE CASCADE?
+        )
+        batch_op.drop_column("username")
+
+    # 5. Create a FK to user id in the "oauth_code" table.
+    # 5.1. Create a new "user_id" column in "oauth_code" and fill its values based on the
+    # "username" column.
+    _add_user_id_col("oauth_code")
+
+    # 5.2. Create FK to the user id in "oauth_code", drop the "username" column.
+    with op.batch_alter_table("oauth_code") as batch_op:
+        batch_op.create_foreign_key(
+            "fk_oauth_code_user_id",
+            "user",
+            ["user_id"],
+            ["id"],
+            # ondelete="CASCADE"  # TODO: Do we want ON DELETE CASCADE?
+        )
         batch_op.drop_column("username")
 
 
 def downgrade():
     user_table = sa.table("user", sa.column("id"), sa.column("name"))
 
-    # # 3. Revert the change (username -> user_id) in "submission".
-    op.add_column("submission", sa.Column("username", sa.String(length=255), nullable=True))
+    def _add_username_col(table_name: str) -> None:
+        """A helper method that adds a `username` column and fills it based on `user_id`."""
+        op.add_column(table_name, sa.Column("username", sa.String(length=255), nullable=True))
+        table = sa.table(table_name, sa.column("username"), sa.column("user_id"))
+        user_subq = (
+            sa.select(user_table.c.name).where(user_table.c.id == table.c.user_id).scalar_subquery()
+        )
+        op.execute(table.update().values(username=user_subq))
+        with op.batch_alter_table(table_name) as batch_op:
+            batch_op.alter_column("username", nullable=False)
 
-    submission_table = sa.table("submission", sa.column("username"), sa.column("user_id"))
-    user_subq = (
-        sa.select(user_table.c.name)
-        .where(user_table.c.id == submission_table.c.user_id)
-        .scalar_subquery()
-    )
-    op.execute(submission_table.update().values(username=user_subq))
+    # # 5. Revert the change (username -> user_id) in "oauth_code".
+    _add_username_col("oauth_code")
+
+    with op.batch_alter_table("oauth_code") as batch_op:
+        batch_op.create_foreign_key(
+            "fk_oauth_code_username",
+            "user",
+            ["user_id"],
+            ["id"],
+            # ondelete="CASCADE"  # TODO: Do we want ON DELETE CASCADE?
+        )
+        batch_op.drop_column("user_id")
+
+    # # 4. Revert the change (username -> user_id) in "api_token".
+    _add_username_col("api_token")
+
+    with op.batch_alter_table("api_token") as batch_op:
+        batch_op.create_foreign_key(
+            "fk_api_token_username",
+            "user",
+            ["user_id"],
+            ["id"],
+            # ondelete="CASCADE"  # TODO: Do we want ON DELETE CASCADE?
+        )
+        batch_op.drop_column("user_id")
+
+    # # 3. Revert the change (username -> user_id) in "submission".
+    _add_username_col("submission")
 
     with op.batch_alter_table("submission") as batch_op:
         batch_op.create_foreign_key(
@@ -99,19 +159,10 @@ def downgrade():
             ["id"],
             # ondelete="CASCADE"  # TODO: Do we want ON DELETE CASCADE?
         )
-        batch_op.alter_column("username", nullable=False)
         batch_op.drop_column("user_id")
 
     # 2. Revert the change (username -> user_id) in "takepart".
-    op.add_column("takepart", sa.Column("username", sa.String(length=255), nullable=True))
-
-    takepart_table = sa.table("takepart", sa.column("username"), sa.column("user_id"))
-    user_subq = (
-        sa.select(user_table.c.name)
-        .where(user_table.c.id == takepart_table.c.user_id)
-        .scalar_subquery()
-    )
-    op.execute(takepart_table.update().values(username=user_subq))
+    _add_username_col("takepart")
 
     with op.batch_alter_table("takepart") as batch_op:
         batch_op.create_foreign_key(
@@ -121,7 +172,6 @@ def downgrade():
             ["id"],
             # ondelete="CASCADE"  # TODO: Do we want ON DELETE CASCADE?
         )
-        batch_op.alter_column("username", nullable=False)
         batch_op.create_primary_key("pk_takepart", ["username", "lectid"])
         batch_op.drop_column("user_id")
 
