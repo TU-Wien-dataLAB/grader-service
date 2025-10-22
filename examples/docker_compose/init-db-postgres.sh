eb8feb309e70b1eccfabb2378fed98169c8a2a32
Natalia <natalia.maniakowska@tuwien.ac.at>
2025-10-22 17:33:46 +0200

    chore: Make the various init bash scripts for containers more consistent

--- examples/docker_compose/init-db-postgres.sh
+++ examples/docker_compose/init-db-postgres.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
-# Initialize a PostgreSQL database: run migrations and load initial data.
 set -e
 
-/venv/bin/grader-service-migrate -f /app/grader_service_config.py
+echo "Initialize a PostgreSQL database: run migrations and load initial data."
+
+/app/.venv/bin/grader-service-migrate -f /app/grader_service_config.py
 # Try to load the initial data. This will fail on the first error (e.g. if the data already exists)
 # and roll back the transaction.
-psql -v ON_ERROR_STOP=1 -h postgres -U postgres -d grader --single-transaction -f /app/data_only.sql
+if ! psql -v ON_ERROR_STOP=1 -h postgres -U postgres -d grader --single-transaction -f /app/data_only.sql; then
+  echo "Initial data import failed; probably the database already exists and contains the data."
+fi
--- examples/docker_compose/init-db.sh
+++ examples/docker_compose/init-db.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
-# Initialize an sqlite database: run migrations and load initial data.
 set -e
+
+echo "Initialize an sqlite database: run migrations and load initial data."
 
 grader-service-migrate -f /app/grader_service_config.py
 # Insert some initial data into the database. The import will fail if the data already
 #  has been inserted, so we ignore errors and just continue.
-sqlite3 /app/service_dir/grader.db < /app/data_only.sql || true
+# Note: For debugging, remove the stderr redirection: `2>/dev/null`.
+if ! sqlite3 /app/service_dir/grader.db < /app/data_only.sql 2>/dev/null; then
+  echo "Initial data import failed; probably the database already exists and contains the data."
+fi
