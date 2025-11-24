2aa5c9546502db4d88754cda0ace606db09675e8
nadja-jovancevic <nadja.jovancevic@tuwien.ac.at>
2025-11-24 14:39:43 +0100

    refactor: assign min/max values to correct variable

--- grader_service/autograding/local_grader.py
+++ grader_service/autograding/local_grader.py
@@ -489,17 +489,17 @@ class LocalAutogradeExecutor(LoggingConfigurable):
                 f"Found custom cell timeout in assignment settings: {custom_cell_timeout} seconds."
             )
             if custom_cell_timeout < self.min_cell_timeout:
-                custom_cell_timeout = self.min_cell_timeout
+                cell_timeout = self.min_cell_timeout
                 self.log.info(
                     f"Custom cell timeout is smaller than the minimum, setting it to the minimum value: {custom_cell_timeout}."
                 )
             elif custom_cell_timeout > self.max_cell_timeout:
-                custom_cell_timeout = self.max_cell_timeout
+                cell_timeout = self.max_cell_timeout
                 self.log.info(
                     f"Custom cell timeout is bigger than the maximum, setting it to the maximum value: {custom_cell_timeout}."
                 )
-
-            cell_timeout = custom_cell_timeout
+            else:
+                cell_timeout = custom_cell_timeout
 
         return cell_timeout
 
