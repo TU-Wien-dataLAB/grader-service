07de636bf166d56cc131659feeeb43624025f5e6
Natalia <natalia.maniakowska@tuwien.ac.at>
2026-03-18 19:23:40 +0100

    cleanup: Remove/reduce outputs from test notebooks. Adjust LimitOutput preprocessor's tests.
    The size of some of them was quite big.

--- grader_service/tests/convert/preprocessors/test_limitoutput.py
+++ grader_service/tests/convert/preprocessors/test_limitoutput.py
@@ -1,15 +1,8 @@
 import os
 
-import pytest
-
 from grader_service.convert.preprocessors import LimitOutput
 
 from .base import BaseTestPreprocessor
-
-
-@pytest.fixture
-def preprocessor():
-    return LimitOutput()
 
 
 class TestLimitOutput(BaseTestPreprocessor):
@@ -17,21 +10,21 @@ class TestLimitOutput(BaseTestPreprocessor):
         nb = self._read_nb(os.path.join("files", "long-output.ipynb"))
         (cell,) = nb.cells
         (output,) = cell.outputs
-        assert len(output.text.split("\n")) > 1000
+        assert len(output.text.split("\n")) > 10
 
-        pp = LimitOutput()
+        pp = LimitOutput(max_lines=10)
         nb, resources = pp.preprocess(nb, {})
 
         (cell,) = nb.cells
         (output,) = cell.outputs
-        assert len(output.text.split("\n")) == 1000
+        assert len(output.text.split("\n")) == 10
 
     def test_infinite_recursion(self):
         nb = self._read_nb(os.path.join("files", "infinite-recursion.ipynb"))
 
-        pp = LimitOutput()
+        pp = LimitOutput(max_traceback=5)
         nb, resources = pp.preprocess(nb, {})
 
         (cell,) = nb.cells
         (output,) = cell.outputs
-        assert len(output.traceback) == 100
+        assert len(output.traceback) == 5
