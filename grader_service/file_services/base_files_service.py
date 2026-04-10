d8c61861c5da5d80de26465116744382d5c80dbe
Natalia <natalia.maniakowska@tuwien.ac.at>
2026-04-10 19:00:49 +0200

    chore: Add docstrings in base file service class

--- grader_service/file_services/base_files_service.py
+++ grader_service/file_services/base_files_service.py
@@ -24,18 +24,22 @@ class BaseFileService(abc.ABC):
 
     @abc.abstractmethod
     def edit_submission(self, submission: Submission) -> None:
+        """Create or overwrite (reset) the instructor's changes to submission files."""
         raise NotImplementedError()
 
     @abc.abstractmethod
     def delete_lecture_files(self, lecture: Lecture) -> None:
+        """Delete all associated files when a lecture is hard-deleted."""
         raise NotImplementedError()
 
     @abc.abstractmethod
     def delete_assignment_files(self, assignment: Assignment, lecture: Lecture) -> None:
+        """Delete all associated files when an assignment is hard-deleted."""
         raise NotImplementedError()
 
     @abc.abstractmethod
     def delete_submission_files(self, submission: Submission) -> None:
+        """Delete all associated files when a submission is hard-deleted."""
         raise NotImplementedError()
 
 
