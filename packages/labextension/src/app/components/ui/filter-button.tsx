3aa67eba2c2224584619e18d228866665441224f
Nadja Jovancevic <e12121824@student.tuwien.ac.at>
2026-08-19 12:42:40 +0200

    fix: remove local interface definitions

--- packages/labextension/src/app/components/ui/filter-button.tsx
+++ packages/labextension/src/app/components/ui/filter-button.tsx
@@ -10,7 +10,6 @@ import {
   DropdownMenuLabel,
   DropdownMenuTrigger
 } from '../../shadcn-components/ui/dropdown-menu';
-import { IFilterGroup } from '../../pages/instructor-view/lecture';
 
 export interface IFilterOption<V extends string = string> {
   value: V;
--- packages/labextension/src/app/pages/instructor-view/lecture.tsx
+++ packages/labextension/src/app/pages/instructor-view/lecture.tsx
@@ -48,15 +48,6 @@ export interface IAssignmentChecked {
   checked: boolean;
 }
 
-interface IFilterOption<V extends string = string> {
-  value: V;
-  label: string;
-}
-
-export interface IFilterGroup {
-  [category: string]: IFilterOption[];
-}
-
 interface IGroupsContextValue {
   groups: string[];
   customGroups: string[];
